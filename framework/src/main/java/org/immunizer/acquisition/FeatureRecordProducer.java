d29b5f786a7b4b6345472e8a615c3cda661902da
Omar Iraqi <o.iraqi@gmail.com>
2020-01-07 16:55:10 +0100

    added more classes

---
+++ framework/src/main/java/org/immunizer/acquisition/FeatureRecordProducer.java
@@ -0,0 +1,34 @@
+package org.immunizer.acquisition;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class FeatureRecordProducer {
+
+    private static FeatureRecordProducer singleton;
+    private KafkaProducer<String, FeatureRecord> producer;
+    private static final String BOOTSTRAP_SERVERS = "kafka-container:9092";
+    private static final String TOPIC = "FeatureRecords";
+
+    private FeatureRecordProducer() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", BOOTSTRAP_SERVERS);
+        props.put("acks", "all");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.immunizer.instrumentation.FeatureRecordSerializer");
+        producer = new KafkaProducer<String, FeatureRecord>(props);
+    }
+
+    public static FeatureRecordProducer getSingleton() {
+        if (singleton == null) {
+            singleton = new FeatureRecordProducer();
+        }
+        return singleton;
+    }
+
+    public void send(FeatureRecord featureRecord) {
+        producer.send(new ProducerRecord<String, FeatureRecord>(TOPIC, featureRecord));
+    }
+}
---
+++ framework/src/main/java/org/immunizer/acquisition/FeatureRecordSerializer.java
@@ -0,0 +1,16 @@
+package org.immunizer.acquisition;
+
+import org.apache.kafka.common.serialization.Serializer;
+import com.google.gson.Gson;
+
+public class FeatureRecordSerializer implements Serializer<FeatureRecord> {
+
+    private Gson gson = new Gson();
+
+    public FeatureRecordSerializer() {}
+    
+    @Override
+    public byte[] serialize(String topic, FeatureRecord featureRecord) {
+        return gson.toJson(featureRecord).getBytes();
+    }
+}
