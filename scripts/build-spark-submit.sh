db873cf0733ae7c4ffa37810f428551cd2dd2404
Omar Iraqi <o.iraqi@gmail.com>
2020-03-01 00:55:20 +0100

    updates

--- scripts/build-spark-submit.sh
+++ scripts/build-spark-submit.sh
@@ -2,4 +2,4 @@ cd ../framework
 ./gradlew libs
 cp build/libs/libs-1.0.jar /root/data/
 cd ../../spark-2.4.5-bin-hadoop2.7/bin
-./spark-submit --class org.immunizer.monitor.MonitorApplication --master spark://spark-master:7077 --deploy-mode cluster --conf spark.driver.userClassPathFirst=true --conf spark.executor.userClassPathFirst=true /data/libs-1.0.jar --jars /data/libs-1.0.jar
+./spark-submit --class org.immunizer.monitor.MonitorApplication --master spark://spark-master:7077 --deploy-mode cluster --conf spark.driver.userClassPathFirst=true /data/libs-1.0.jar --jars /data/libs-1.0.jar
