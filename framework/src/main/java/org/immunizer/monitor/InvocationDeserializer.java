10cd0feb9e2aeefb3a82294ae39b12810852b6ff
Omar Iraqi <o.iraqi@gmail.com>
2020-02-29 19:48:05 +0100

    updates

--- framework/src/main/java/org/immunizer/monitor/InvocationDeserializer.java
+++ framework/src/main/java/org/immunizer/monitor/InvocationDeserializer.java
@@ -3,7 +3,7 @@ package org.immunizer.monitor;
 import org.apache.kafka.common.serialization.Deserializer;
 
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;;
+import com.google.gson.JsonParser;
 
 public class InvocationDeserializer implements Deserializer<JsonObject> {
 
