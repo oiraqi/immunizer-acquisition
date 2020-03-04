9de0c90b29def2262e6ee6f1e64a695bc35754b8
Omar Iraqi <o.iraqi@gmail.com>
2020-03-04 19:55:56 +0100

    added Ignite support

---
+++ framework/src/main/java/org/immunizer/monitor/DistributedCache.java
@@ -0,0 +1,79 @@
+package org.immunizer.monitor;
+
+import org.apache.ignite.spark.JavaIgniteContext;
+import org.apache.ignite.spark.JavaIgniteRDD;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import org.apache.spark.util.StatCounter;
+
+import scala.Tuple2;
+
+public class DistributedCache {
+
+    private JavaIgniteContext<String, Double> igniteContext1;
+    private JavaIgniteContext<String, Integer> igniteContext2;
+    private JavaIgniteRDD<String, Double> stdevsRDD;
+    private JavaIgniteRDD<String, Double> meansRDD;
+    private JavaIgniteRDD<String, Integer> pathsRDD;
+    private JavaIgniteRDD<String, Integer> splits1RDD;
+    private JavaIgniteRDD<String, Integer> splits3RDD;
+
+    public DistributedCache(JavaSparkContext sc) {
+        igniteContext1 = new JavaIgniteContext<String, Double>(sc, "examples/config/spark/example-shared-rdd.xml");
+        igniteContext2 = new JavaIgniteContext<String, Integer>(sc, "examples/config/spark/example-shared-rdd.xml");
+
+        stdevsRDD = igniteContext1.fromCache("stdevsRDD");
+        meansRDD = igniteContext1.fromCache("meansRDD");
+        pathsRDD = igniteContext2.fromCache("pathsRDD");
+        splits1RDD = igniteContext2.fromCache("splits1RDD");
+        splits3RDD = igniteContext2.fromCache("splits3RDD");
+    }
+
+    public void saveModel(JavaRDD<String> model) {
+        JavaPairRDD<String, StatCounter> numbersModel = model.filter(record -> record.startsWith("numbers_"))
+                .mapToPair(record -> {
+                    String key = record.substring(0, record.lastIndexOf('_'));
+                    Double value = Double.valueOf(record.substring(record.lastIndexOf('_') + 1));
+                    return new Tuple2<String, Double>(key, value);
+                }).aggregateByKey(new StatCounter(), StatCounter::merge, StatCounter::merge);
+        JavaPairRDD<String, Double> stdevsModel = numbersModel
+                .mapToPair(stats -> new Tuple2<String, Double>(stats._1(), stats._2().stdev()));
+        JavaPairRDD<String, Double> meansModel = numbersModel
+                .mapToPair(stats -> new Tuple2<String, Double>(stats._1(), stats._2().mean()));
+
+        JavaPairRDD<String, Integer> pathsModel = model.filter(record -> record.startsWith("paths_"))
+                .mapToPair(record -> new Tuple2<String, Integer>(record, 1)).reduceByKey((a, b) -> a + b);
+
+        JavaPairRDD<String, Integer> splits1Model = model.filter(record -> record.startsWith("splits_1_"))
+                .mapToPair(record -> new Tuple2<String, Integer>(record, 1)).reduceByKey((a, b) -> a + b);
+
+        JavaPairRDD<String, Integer> splits3Model = model.filter(record -> record.startsWith("splits_3_"))
+                .mapToPair(record -> new Tuple2<String, Integer>(record, 1)).reduceByKey((a, b) -> a + b);
+
+        stdevsModel.foreach(entry -> {
+            System.out.println(entry._1() + ": " + entry._2());
+        });
+
+        meansModel.foreach(entry -> {
+            System.out.println(entry._1() + ": " + entry._2());
+        });
+
+        pathsModel.foreach(entry -> {
+            System.out.println(entry._1() + ": " + entry._2());
+        });
+        splits1Model.foreach(entry -> {
+            System.out.println(entry._1() + ": " + entry._2());
+        });
+        splits3Model.foreach(entry -> {
+            System.out.println(entry._1() + ": " + entry._2());
+        });
+
+        stdevsRDD.savePairs(stdevsModel);
+        meansRDD.savePairs(meansModel);
+        pathsRDD.savePairs(pathsModel);
+        splits1RDD.savePairs(splits1Model);
+        splits3RDD.savePairs(splits3Model);
+    }
+}
--- framework/src/main/java/org/immunizer/monitor/MonitorApplication.java
+++ framework/src/main/java/org/immunizer/monitor/MonitorApplication.java
@@ -2,18 +2,11 @@ package org.immunizer.monitor;
 
 import java.util.Vector;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.util.StatCounter;
 
-import scala.Tuple2;
-import scala.Tuple3;
-
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.SparkConf;
 
 import java.time.Duration;
-
-import com.google.gson.JsonObject;
 
 public class MonitorApplication {
 
@@ -22,49 +15,14 @@ public class MonitorApplication {
         SparkConf conf = new SparkConf().setAppName("Monitor").setMaster("spark://spark-master:7077");
         JavaSparkContext sc = new JavaSparkContext(conf);
         InvocationConsumer consumer = new InvocationConsumer();
-        /*
-         * FeatureExtractor extractor = FeatureExtractor.getSingleton();
-         * FeatureRecordProducer producer = new FeatureRecordProducer();
-         */
+        DistributedCache cache = new DistributedCache(sc);
 
         try {
             while (true) {
                 Vector<byte[]> records = consumer.poll(Duration.ofSeconds(60));
                 JavaRDD<byte[]> rdd = sc.parallelize(records);
-
                 JavaRDD<String> model = rdd.flatMap(new ModelMapper());
-
-                JavaRDD<Tuple3<String, Double, Double>> numbersModel = model
-                        .filter(record -> record.startsWith("numbers_")).mapToPair(record -> {
-                            String key = record.substring(0, record.lastIndexOf('_'));
-                            Double value = Double.valueOf(record.substring(record.lastIndexOf('_') + 1));
-                            return new Tuple2<String, Double>(key, value);
-                        }).aggregateByKey(new StatCounter(), StatCounter::merge, StatCounter::merge)
-                        .map(stats -> new Tuple3<String, Double, Double>(stats._1(), stats._2().stdev(),
-                                stats._2().mean()));
-
-                JavaPairRDD<String, Integer> pathsModel = model.filter(record -> record.startsWith("paths_"))
-                        .mapToPair(record -> new Tuple2<String, Integer>(record, 1)).reduceByKey((a, b) -> a + b);
-
-                JavaPairRDD<String, Integer> splits1Model = model.filter(record -> record.startsWith("splits_1_"))
-                        .mapToPair(record -> new Tuple2<String, Integer>(record, 1)).reduceByKey((a, b) -> a + b);
-
-                JavaPairRDD<String, Integer> splits3Model = model.filter(record -> record.startsWith("splits_3_"))
-                        .mapToPair(record -> new Tuple2<String, Integer>(record, 1)).reduceByKey((a, b) -> a + b);
-
-                numbersModel.foreach(entry -> {
-                    System.out.println(entry._1() + ": " + entry._2() + " | " + entry._3());
-                });
-                
-                pathsModel.foreach(entry -> {
-                    System.out.println(entry._1() + ": " + entry._2());
-                });
-                splits1Model.foreach(entry -> {
-                    System.out.println(entry._1() + ": " + entry._2());
-                });
-                splits3Model.foreach(entry -> {
-                    System.out.println(entry._1() + ": " + entry._2());
-                });
+                cache.saveModel(model);
             }
         } finally {
             consumer.close();
