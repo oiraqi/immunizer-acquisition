32b7ae4f00f7c7ee8bf8db4e0420566ace4c11a3
Omar Iraqi <o.iraqi@gmail.com>
2020-03-05 05:22:09 +0100

    updates

--- framework/src/main/java/org/immunizer/monitor/DistributedCache.java
+++ framework/src/main/java/org/immunizer/monitor/DistributedCache.java
@@ -34,7 +34,7 @@ public class DistributedCache {
     public void saveModel(JavaRDD<String> model) {
         JavaPairRDD<String, StatCounter> numbersModel = model.filter(record -> record.startsWith("numbers_"))
                 .mapToPair(record -> {
-                    String key = record.substring(0, record.lastIndexOf('_'));
+                    String key = record.substring(8, record.lastIndexOf('_'));
                     Double value = Double.valueOf(record.substring(record.lastIndexOf('_') + 1));
                     return new Tuple2<String, Double>(key, value);
                 }).aggregateByKey(new StatCounter(), StatCounter::merge, StatCounter::merge);
@@ -44,30 +44,28 @@ public class DistributedCache {
                 .mapToPair(stats -> new Tuple2<String, Double>(stats._1(), stats._2().mean()));
 
         JavaPairRDD<String, Integer> pathsModel = model.filter(record -> record.startsWith("paths_"))
-                .mapToPair(record -> new Tuple2<String, Integer>(record, 1)).reduceByKey((a, b) -> a + b);
+                .mapToPair(record -> new Tuple2<String, Integer>(record.substring(6), 1)).reduceByKey((a, b) -> a + b);
 
         JavaPairRDD<String, Integer> splits1Model = model.filter(record -> record.startsWith("splits_1_"))
-                .mapToPair(record -> new Tuple2<String, Integer>(record, 1)).reduceByKey((a, b) -> a + b);
+                .mapToPair(record -> new Tuple2<String, Integer>(record.substring(9), 1)).reduceByKey((a, b) -> a + b);
 
         JavaPairRDD<String, Integer> splits3Model = model.filter(record -> record.startsWith("splits_3_"))
-                .mapToPair(record -> new Tuple2<String, Integer>(record, 1)).reduceByKey((a, b) -> a + b);
+                .mapToPair(record -> new Tuple2<String, Integer>(record.substring(9), 1)).reduceByKey((a, b) -> a + b);
 
         stdevsModel.foreach(entry -> {
-            System.out.println(entry._1() + ": " + entry._2());
+            System.out.println("STDEV: " + entry._1() + ": " + entry._2());
         });
-
         meansModel.foreach(entry -> {
-            System.out.println(entry._1() + ": " + entry._2());
+            System.out.println("MEAN: " + entry._1() + ": " + entry._2());
         });
-
         pathsModel.foreach(entry -> {
-            System.out.println(entry._1() + ": " + entry._2());
+            System.out.println("PATH: " + entry._1() + ": " + entry._2());
         });
         splits1Model.foreach(entry -> {
-            System.out.println(entry._1() + ": " + entry._2());
+            System.out.println("SPLIT1: " + entry._1() + ": " + entry._2());
         });
         splits3Model.foreach(entry -> {
-            System.out.println(entry._1() + ": " + entry._2());
+            System.out.println("SPLIT3: " + entry._1() + ": " + entry._2());
         });
 
         stdevsRDD.savePairs(stdevsModel);
