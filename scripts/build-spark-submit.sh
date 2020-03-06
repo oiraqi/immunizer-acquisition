c9f8028a215f0798564542c1a7032b9cddb80f2f
Omar Iraqi <o.iraqi@gmail.com>
2020-03-06 12:12:06 +0100

    updates

--- scripts/build-spark-submit.sh
+++ scripts/build-spark-submit.sh
@@ -1,5 +1,7 @@
 cd ../framework
 ./gradlew build
+./gradlew libs
 cp build/libs/framework-1.0.jar /root/data/
+cp build/libs/libs-1.0.jar /root/data/
 cd ../../spark-2.4.5-bin-hadoop2.7/bin
-./spark-submit --class org.immunizer.monitor.MonitorApplication --master spark://spark-master:7077 --deploy-mode cluster /data/framework-1.0.jar --jars /data/framework-1.0.jar
+./spark-submit --class org.immunizer.monitor.MonitorApplication --master spark://spark-master:7077 --deploy-mode cluster /data/framework-1.0.jar --jars /data/libs-1.0.jar
