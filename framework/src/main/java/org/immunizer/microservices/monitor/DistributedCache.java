eeb93568cc1fa07fe64d3321d70b6641ae8980d2
Omar Iraqi <o.iraqi@gmail.com>
2020-12-30 15:11:24 +0100

    updates

--- framework/src/main/java/org/immunizer/microservices/monitor/DistributedCache.java
+++ framework/src/main/java/org/immunizer/microservices/monitor/DistributedCache.java
@@ -5,7 +5,6 @@ import org.apache.ignite.spark.JavaIgniteRDD;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-
 import org.apache.spark.util.StatCounter;
 
 import scala.Tuple2;
