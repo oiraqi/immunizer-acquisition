7f0325e4f4f711f13c393eca4f22f8d852840064
Omar Iraqi <o.iraqi@gmail.com>
2021-02-26 18:56:57 +0100

    added Kafka streaming

--- framework/src/main/java/org/immunizer/microservices/monitor/Monitor.java
+++ framework/src/main/java/org/immunizer/microservices/monitor/Monitor.java
@@ -1,28 +1,45 @@
 package org.immunizer.microservices.monitor;
 
-import java.util.Vector;
-import java.time.Duration;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.api.java.*;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.regex.Pattern;
 
 public class Monitor {
+    private static final String TOPIC_PATTERN = "Invocations/.+";
+    private static final String BOOTSTRAP_SERVERS = "kafka:9092";
+    private static final String GROUP_ID = "Monitor";
 
-    public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName("Monitor").setMaster("spark://spark-master:7077");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        InvocationConsumer consumer = new InvocationConsumer();
-        DistributedCache cache = new DistributedCache(sc);
+    public static void main(String[] args) throws Exception {
+        SparkConf sparkConf = new SparkConf().setAppName("Monitor").setMaster("spark://spark-master:7077");
+        JavaStreamingContext jsc = new JavaStreamingContext(sparkConf, Durations.seconds(60));
 
-        try {
-            while (true) {
-                Vector<byte[]> records = consumer.poll(Duration.ofSeconds(60));
-                JavaRDD<String> model = sc.parallelize(records).flatMap(new ModelMapper()).filter(record -> record != null);
-                cache.updateModel(model);
-            }
-        } finally {
-            consumer.close();
-            sc.close();
-        }
+        Map<String, Object> kafkaParams = new HashMap<>();
+        kafkaParams.put("bootstrap.servers", BOOTSTRAP_SERVERS);
+        kafkaParams.put("group.id", GROUP_ID);
+        kafkaParams.put("enable.auto.commit", "true");
+        kafkaParams.put("auto.commit.interval.ms", "1000");
+        kafkaParams.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        kafkaParams.put("value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
+
+        JavaInputDStream<ConsumerRecord<String, byte[]>> invocationStream = 
+            KafkaUtils.createDirectStream(jsc, LocationStrategies.PreferConsistent(),
+            ConsumerStrategies.SubscribePattern(Pattern.compile(TOPIC_PATTERN), kafkaParams));
+
+        JavaDStream<String> modelStream = invocationStream.map(ConsumerRecord::value).flatMap(new ModelMapper()).filter(record -> record != null);
+
+        DistributedCache cache = new DistributedCache(jsc.sparkContext());
+        modelStream.foreachRDD(model -> cache.updateModel(model));
+
+        jsc.start();
+        jsc.awaitTermination();
     }
 }
